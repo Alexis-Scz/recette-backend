a557291e0c7e9d9952e906ca8c88688c5229738c
Alexis-Scz <alexissoucaze@yahoo.Fr>
2024-11-27 21:59:09 +0100

    "env"

--- config/database.config.js
+++ config/database.config.js
@@ -1,11 +1,17 @@
 import pkg from "pg";
+import dotenv from "dotenv"
 
+
+dotenv.config()
+
+
+console.log("xvgbch"+ process.env.DB_HOST)
 const {Pool } = pkg
 export const pool = new Pool({
-     host: '192.168.37.133', 
-     user:'postgres', 
-     password: 'password',
-     port: 5432,
-     database: "bddrct"
+     host: process.env.DB_HOST,
+     user: process.env.DB_USER,
+     password: process.env.DB_PASSWORD,
+     database: process.env.DB_NAME,
+     port: process.env.DB_PORT,
 });
 pool.connect()
